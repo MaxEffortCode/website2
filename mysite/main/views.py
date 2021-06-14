c4e1ac3d05b06f44dd7f6a232d28980770ef96d1
ThiccusBootieus <bultman82@gmail.com>
2021-06-13 21:06:04 -0500

    Update views.py

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import ToDoList, Item
+from .forms import CreateNewList
 # Create your views here.
 
+def index(response, id):
+	ls = ToDoList.objects.get(id=id)
+	#item = ls.item_set.get(id=1)
+	#return render(response, "main/base.html", {"name":ls.name})
+	return render(response, "main/list.html", {"ls":ls})
 
-def index(response):
-	return HttpResponse("<h1> this is where html goes </h1>")
+
+def v1(response):
+	return HttpResponse("<h1> v1 </h1>")
+
+
+def home(response):
+	return render(response, "main/home.html", {})
+
+def create(response): #response takes in the html request types "get", "post", ect. defaults to "GET"
+	if response.method == "POST":
+		form = CreateNewList(response.POST)
+
+		if form.is_valid():
+			#grabs data from field "name"
+			n = form.cleaned_data["name"]
+			t = ToDoList(name=n)
+	else:
+		form = CreateNewList()
+	return render(response, "main/create.html", {"form":form})
+
