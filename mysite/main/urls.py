ee71df11d6b8ac7815523b3843d537f6dce19d67
ThiccusBootieus <bultman82@gmail.com>
2021-06-13 21:06:38 -0500

    Update urls.py

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -3,5 +3,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path("", views.index, name="index"),#""<- directory in url, views.index is the file served, name="index" is function ran from views.py
+#we are passing the int:id to views.py
+
+
+
+path("<int:id>", views.index, name="index"),
+
+path("", views.index, name ="index"),
+path("home/", views.home, name ="home"),
+path("create/", views.create, name="create"),
+
+#""directory in url, views.v1 is the function v1() ran from views.py dont know what name is for
+path("v1/", views.v1, name="view 1"),
+
 ]
