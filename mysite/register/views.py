081d59ba920790f9e13f2ed7904c2bae94d24792
ThiccusBootieus <bultman82@gmail.com>
2021-06-25 23:17:46 -0500

    Update views.py

--- mysite/register/views.py
+++ mysite/register/views.py
@@ -4,6 +4,7 @@ from .forms import RegisterForm
 
 
 # Create your views here.
+# TODO Create a deletion option
 def register(response):
 	if response.method == "POST":
 		form = RegisterForm(response.POST)
