0333f14bfcfd2f8a556e3802475d4a3b9f54a682
ThiccusBootieus <bultman82@gmail.com>
2021-06-29 00:37:34 -0500

    Update urls.py

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -2,19 +2,33 @@ from django.urls import path
 
 from . import views
 
+
+# Django provides base view classes which will suit a wide range of applications. All views inherit from the View 
+# class, which handles linking the view into the URLs, HTTP method dispatching and other common features. 
+# RedirectView provides a HTTP redirect, and TemplateView extends the base class to make it also render a template.
+
+app_name = 'main'
 urlpatterns = [
-#we are passing the int:id to views.py
+
 
 
 
 path("<int:id>", views.index, name="index"),
 
-path("", views.index, name ="index"),
+path("vote/", views.view, name="vote"),
+
+path("vote/<int:list_id>", views.vote, name="vote"),
+
+
+
+
+path("", views.home, name ="home"),
 path("home/", views.home, name ="home"),
 path("create/", views.create, name="create"),
 
 #""directory in url, views.v1 is the function v1() ran from views.py dont know what name is for
 path("v1/", views.v1, name="view 1"),
 path("view/", views.view, name="view"),
+path('<int:item_id>/vote/', views.vote, name='vote'),
 
 ]
