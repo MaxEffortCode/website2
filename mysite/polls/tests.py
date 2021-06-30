99b3c5ca2bc55e41c591a0084d279a3bbe6b9ce8
Max B <bultman82@gmail.com>
2021-06-30 17:28:20 -0500

    Update tests.py

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+import datetime
 
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
+
+class QuestionModelTests(TestCase):
+
+	def test_was_published_recently_with_future_question(self):
+		time = timezone.now() + datetime.timedelta(days=30)
+		future_question = Question(pub_date=time)
+		try:
+			self.assertIs(future_question.was_published_recently(), False)
+		except AssertionError:
+			print("\n\n\n******TEST FAILURE:******\n")
+			print("future date should not be considered published recently\n\n")
 # Create your tests here.
