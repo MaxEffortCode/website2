76d7792b6c61001e86cf549e4fe2bbb816573469
Max B <bultman82@gmail.com>
2021-07-01 00:29:22 -0400

    Add files via upload

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
 	model = Choice
 	extra = 3
 
@@ -13,5 +13,8 @@ class QuestionAdmin(admin.ModelAdmin):
 		('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
 	]
 	inlines = [ChoiceInline]
+	list_filter = ['pub_date']
+	search_fields = ['question_text']
+	list_display = ('question_text', 'pub_date', 'was_published_recently')
 
 admin.site.register(Question, QuestionAdmin)
--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'),
-     # ex: /polls/5/
-    path('<int:pk>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    path('<int:pk>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+	path('', views.IndexView.as_view(), name='index'),
+	 # ex: /polls/5/
+	path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+	# ex: /polls/5/results/
+	path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+	# ex: /polls/5/vote/
+	path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
