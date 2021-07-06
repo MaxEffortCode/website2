a84ac2ad33c167384cdb45c928e04de8950c4284
Max B <bultman82@gmail.com>
2021-07-06 15:47:15 -0500

    Delete mysite directory

--- mysite/main/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class CreateNewList(forms.Form):
-	#the fields in the forum
-	name = forms.CharField(label="Name", max_length=200)
-	check = forms.BooleanField(required=False)
-
