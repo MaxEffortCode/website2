18088d51f768b27e723cde2249daf26949d8ac72
Max B <bultman82@gmail.com>
2021-07-11 16:18:57 -0500

    Update views.py

--- django/mineapp/views.py
+++ django/mineapp/views.py
@@ -7,6 +7,23 @@ from django.utils import timezone
 from .models import ToDoList, Item
 
 
+def index(request):
+    latest_question_list = ToDoList.objects.order_by('-name')[:5]
+    output = ', '.join([q.name for q in latest_question_list])
+    return HttpResponse(output)
+
+
+class IndexView(generic.ListView):
+	template_name = 'mineapp/index.html'
+	context_object_name = 'latest_question_list'
+
+	def get_queryset(self):
+		"""Return the last five published questions."""
+		time = timezone.now()
+		#pub_date__lte is less than or equal to '-' in -pub_date is to order from new -> old
+		return ToDoList.objects.filter(name__lte=timezone.now()).order_by('-name')[:10]
+
+
 class DetailView(generic.DetailView):
 	model = ToDoList
 	template_name = 'mineapp/detail.html'
@@ -15,3 +32,7 @@ class DetailView(generic.DetailView):
 		Excludes any questions that aren't published yet.
 		"""
 		return ToDoList.objects.all()
+
+class ResultsView(generic.DetailView):
+	model = ToDoList
+	template_name = 'mineapp/results.html'
